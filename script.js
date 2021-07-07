5def9730d2055c78086b66d27186b7ac1ac918c7
jrjradler <adlerrjake@gmail.com>
2021-07-07 13:54:13 +0400

    Fixes to js

--- script.js
+++ script.js
@@ -5,8 +5,6 @@
 *   commented out for now.
 *   https://kevinhufnagl.com
 */
-
-
 //Converting colors to proper format
 function normalizeColor(hexCode) {
   return [(hexCode >> 16 & 255) / 255, (hexCode >> 8 & 255) / 255, (255 & hexCode) / 255]
@@ -519,3 +517,69 @@ class Gradient {
 */
 var gradient = new Gradient();
     gradient.initGradient("#gradient-canvas");
+    $('.email').on("change keyup paste",
+    function(){
+      if($(this).val()){
+        $('.icon-paper-plane').addClass("next");
+      } else {
+        $('.icon-paper-plane').removeClass("next");
+      }
+    }
+  );
+  
+  $('.next-button').hover(
+    function(){
+      $(this).css('cursor', 'pointer');
+    }
+  );
+  
+  $('.next-button.email').click(
+    function(){
+      console.log("Something");
+      $('.email-section').addClass("fold-up");
+      $('.password-section').removeClass("folded");
+    }
+  );
+  
+  $('.password').on("change keyup paste",
+    function(){
+      if($(this).val()){
+        $('.icon-lock').addClass("next");
+      } else {
+        $('.icon-lock').removeClass("next");
+      }
+    }
+  );
+  
+  $('.next-button').hover(
+    function(){
+      $(this).css('cursor', 'pointer');
+    }
+  );
+  
+  $('.next-button.password').click(
+    function(){
+      console.log("Something");
+      $('.password-section').addClass("fold-up");
+      $('.repeat-password-section').removeClass("folded");
+    }
+  );
+  
+  $('.repeat-password').on("change keyup paste",
+    function(){
+      if($(this).val()){
+        $('.icon-repeat-lock').addClass("next");
+      } else {
+        $('.icon-repeat-lock').removeClass("next");
+      }
+    }
+  );
+  
+  $('.next-button.repeat-password').click(
+    function(){
+      console.log("Something");
+      $('.repeat-password-section').addClass("fold-up");
+      $('.success').css("marginTop", 0);
+    }
+  );
+  
